c861727cb024f4023a5bb4835c8d9e2e6a832814
Trochez <juan.trochez95@gmail.com>
2017-03-21 16:44:41 +0100

    [router] create routes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,29 @@
-var app = angular.module("crmAngular", []);
+var app = angular.module("crmAngular", ["ui.router"]);
+
+
+app.config(function ($stateProvider) {
+    var templateFolder = "app/templates/";
+    
+    var homeState = {
+        name: 'home',
+        url: '/',
+        templateUrl: templateFolder + "index.html"
+    }
+    var clientState = {
+        name: 'clients',
+        url: '/clients',
+        templateUrl: templateFolder + "clients.html"
+    }
+
+    var interactionState = {
+        name: 'interactions',
+        url: '/interactions',
+        templateUrl: templateFolder + "interactions.html"
+    }
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(clientState);
+    $stateProvider.state(interactionState);
+});
 
 
